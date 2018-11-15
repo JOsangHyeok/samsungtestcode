5e91c46a65458c295e7722e09bba5e38e9b684f0
JOsangHyeok <36909783+JOsangHyeok@users.noreply.github.com>
2018-11-15 16:16:08 +0900

    Add files via upload

---
+++ p1936.java
@@ -0,0 +1,21 @@
+package stest;
+
+import java.util.Scanner;
+
+public class p1936 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		 int input1 = scan.nextInt();
+		 int input2 = scan.nextInt();
+		  
+		if(input1>input2) {
+			System.out.println("A");
+		} else {
+			System.out.println("B");
+		}
+			
+
+	}
+
+}
