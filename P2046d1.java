a57bef65c81844b5415730b473eb7dffe5a60151
JOsangHyeok <36909783+JOsangHyeok@users.noreply.github.com>
2018-11-16 20:48:18 +0900

    Add files via upload

---
+++ P2046d1.java
@@ -0,0 +1,18 @@
+package stest; //next again
+
+import java.util.Scanner;
+
+public class P2046d1 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		for(int i=1;i<num+1;i++) {
+			System.out.print("#");
+		}
+		
+		sc.close();
+	
+    }
+}
